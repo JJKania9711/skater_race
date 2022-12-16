0094018b9e98e35f7d015337dcb1bbdb1f04ec3d
JJKania9711 <116130334+JJKania9711@users.noreply.github.com>
2022-12-15 16:43:56 -0800

    Core Project
    Initial projects files

---
+++ skater_race/skater_race/skater_raceApp.swift
@@ -0,0 +1,17 @@
+//
+//  skater_raceApp.swift
+//  skater_race
+//
+//  Created by James on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct skater_raceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
