9a689b8ec9dc0c5d27cff8a6cde6e67e74c4178c
JJKania9711 <116130334+JJKania9711@users.noreply.github.com>
2022-12-18 15:26:28 -0800

    Final edits

--- skater_race/skater_race/ContentView.swift
+++ skater_race/skater_race/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  skater_race: A race game based on the horse racing dice and board game
 //    game play: 1. Pick one of the five skaters to win the race
-//               2. Roll the dice and the first number 1-6 decides which skater moves. If 6 ALL skaters move.
+//               2. Roll the dice and the first number 1-6 decides which skater moves. If 6 is rolled then ALL skaters move.
 //                  The second number of 1-6 decides how far the selected skater moves
 //               3. Keep rolling until at least one skater crosses the finish line
 //
@@ -18,13 +18,14 @@ struct ContentView: View {
     @State var skaterStridesText: String = ""
     @State var skaterSelected: Int = 0
     @State var skaterStrides: Int = 0
+    
     @State var skater1Ypos: CGFloat = -550
     @State var skater2Ypos: CGFloat = -550
     @State var skater3Ypos: CGFloat = -550
     @State var skater4Ypos: CGFloat = -550
     @State var skater5Ypos: CGFloat = -550
     
-    // State variables to show the winners
+    // State variables to show the winners in a new screen
     @State private var showSheet = false
     @State private var strWhoWon: String = ""
     
@@ -85,13 +86,13 @@ struct ContentView: View {
                                 Button(
                                  "Roll",
                                  action: {
-                                   //roll the dice to see which skater moves and how far they move
+                                   //the button rolls the dice to see which skater moves and how far they move
                                    skaterSelected = randomDiceRoll()
                                    skaterStrides = randomDiceRoll()
                                    skaterSelectedText = "Skater " + String(skaterSelected)
                                    skaterStridesText  = "Moves " + String(skaterStrides) + " Strides"
                                    
-                                   //this code block moves the skater
+                                   // depending on the dice values this code moves the skater
                                    let skaterPosChange = CGFloat(skaterStrides * 10)
                                      switch skaterSelected {
                                      case 1:
@@ -115,7 +116,7 @@ struct ContentView: View {
                                      }
                                      
                                      
-                                     // Check to see if any skater crossed the finish line
+                                     // After every move this code checks to see if any skater crossed the finish line
                                       var haveWinner: Bool = false
                                       strWhoWon = ""
                                       haveWinner = checkForWinner(skater1Distance: skater1Ypos, skater2Distance: skater2Ypos, skater3Distance: skater3Ypos, skater4Distance: skater4Ypos, skater5Distance: skater5Ypos, strWinners: &strWhoWon )
@@ -191,18 +192,21 @@ struct SheetContentView: View {
 
 
 
+// This function is called twice by the Roll button action. Once for the skater and once for the distance
 func randomDiceRoll() -> Int {
     // This function returns a random number between 1 and 6
     return Int(arc4random_uniform(6) + 1)
 }
 
 func checkForWinner(skater1Distance: CGFloat, skater2Distance: CGFloat, skater3Distance: CGFloat, skater4Distance: CGFloat, skater5Distance: CGFloat, strWinners: inout String) -> Bool {
-    // This function checks to see which skaters crossed the finish line
+    // This function takes in all 5 skater positions and checks to see which skaters crossed the finish line
     
     // skaters start at a -550 Y position so the finishline is at 0
     let finishLineYPos: CGFloat = 0
     var nbrWinners: Int = 0
     
+    // check each skater to see if they crossed the finishline
+    // I use nbrWinners to increment how many skaters cross the finishline because if it's more than 1 we have a tie
     if skater1Distance > finishLineYPos {
         nbrWinners+=1
         strWinners = "1"
@@ -237,14 +241,19 @@ func checkForWinner(skater1Distance: CGFloat, skater2Distance: CGFloat, skater3D
         nbrWinners+=1
     }
     
+    // The code determines whether or not we have a winner and if we have a winner we determine if it's a single skater or multiple skaters
+  
+    // if it's only 1 winner
     if nbrWinners == 1 {
         strWinners = "Skater " + strWinners + " won the race"
         return true
     }
+    // if there is a tie with more than 1 winner
     else if nbrWinners > 1 {
         strWinners = "Skaters " + strWinners + " won the race"
         return true
     }
+    // we don't have a winner yet
     else {
         return false
     }
